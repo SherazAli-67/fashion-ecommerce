176cd2a1137cf0e0ae8dfe41d41d5c6db70a5a3e
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-21 00:25:12 +0500

    Fashion Ecommerce - App Colors, TextStyles set

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -3,4 +3,8 @@ import 'dart:ui';
 class AppColors {
   static const primaryColor = Color(0xffFF8D41);
   static const unSelectedGreyColor = Color(0xff9E9E9E);
+  static const whiteColor = Color(0xffffffff);
+  static const neutral900 = Color(0xff424242);
+  static const frostedBackFill = Color(0x59ffffff);
+  static const frostedBackBorder = Color(0xccffffff);
 }
--- lib/core/app_textstyles.dart
+++ lib/core/app_textstyles.dart
@@ -1,6 +1,23 @@
-
+import 'package:fashion_ecommerce/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTextStyles {
-  static final navigationTextStyle =  TextStyle(fontSize: 12);
+  static final navigationTextStyle = TextStyle(fontSize: 12);
+
+  static final imageViewTitle = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 14,
+    fontWeight: .w600,
+    color: AppColors.neutral900,
+    letterSpacing: 0.2,
+    height: 1.4,
+  );
+
+  static final imageViewUsername = TextStyle(
+    fontFamily: 'Urbanist',
+    fontSize: 10,
+    fontWeight: .w400,
+    color: AppColors.unSelectedGreyColor,
+    letterSpacing: 0.2,
+  );
 }
