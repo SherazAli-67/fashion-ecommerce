bdbdce9f0d840c984a87ba745842d82c8942f969
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-21 00:09:05 +0500

    Fashion Ecommerce -Model + AppData set

--- lib/core/app_data.dart
+++ lib/core/app_data.dart
@@ -1,3 +1,11 @@
-class AppData {
+import 'package:fashion_ecommerce/core/models/lookbook_image.dart';
 
+class AppData {
+  static const lookbookImages = [
+    LookbookImage(
+      imagePath: 'assets/images/lookbook_image_view.jpg',
+      title: 'Image Name dolor sit amet, consectetur',
+      username: 'Username',
+    ),
+  ];
 }
---
+++ lib/core/models/lookbook_image.dart
@@ -0,0 +1,11 @@
+class LookbookImage {
+  const LookbookImage({
+    required this.imagePath,
+    required this.title,
+    required this.username,
+  });
+
+  final String imagePath;
+  final String title;
+  final String username;
+}
